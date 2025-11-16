504e217cde2a8cbad8152bc7fcadb15b8b69eddd
bhardwajdevesh092005 <b23cs1014@iitj.ac.in>
2025-11-16 18:35:15 +0530

    All Objectives Complete. All tests running

--- bulklayer/bl.c
+++ bulklayer/bl.c
@@ -1,17 +1,54 @@
 #include "bl.h"
 #include "blinternal.h"
 #include <stdio.h>
+#include <string.h>
 
-void BL_Init(void) { PF_Init(); }
-int BL_CreateIndex(char *f) {
-    int fd, err, p; char *b; BL_HeaderPage *h;
-    if(PF_CreateFile(f)!=PFE_OK) return BL_ERROR;
-    if((fd=PF_OpenFile(f,0))<0) return BL_ERROR;
-    if(PF_AllocPage(fd,&p,&b)!=PFE_OK){PF_CloseFile(fd);return BL_ERROR;}
-    h=(BL_HeaderPage*)b;h->rootPage=-1;h->firstLeaf=-1;h->numLevels=0;
-    h->numRecords=0;h->keyType='i';h->keyLength=sizeof(int);
-    PF_UnfixPage(fd,0,TRUE);PF_CloseFile(fd);return BL_OK;
+void BL_Init(void) {
+    PF_Init();
 }
-int BL_DestroyIndex(char *f){return PF_DestroyFile(f)==PFE_OK?BL_OK:BL_ERROR;}
-int BL_OpenIndex(char *f){int fd=PF_OpenFile(f,0);return fd<0?BL_ERROR:fd;}
-int BL_CloseIndex(int fd){return PF_CloseFile(fd)==PFE_OK?BL_OK:BL_ERROR;}
+
+int BL_CreateIndex(char *fileName) {
+    int fd, err, pageNum;
+    char *pageBuf;
+    BL_HeaderPage *header;
+    
+    err = PF_CreateFile(fileName);
+    if (err != PFE_OK) {
+        PF_PrintError("BL_CreateIndex failed");
+        return BL_ERROR;
+    }
+    
+    fd = PF_OpenFile(fileName, 0);
+    if (fd < 0) return BL_ERROR;
+    
+    err = PF_AllocPage(fd, &pageNum, &pageBuf);
+    if (err != PFE_OK) {
+        PF_CloseFile(fd);
+        return BL_ERROR;
+    }
+    
+    header = (BL_HeaderPage*)pageBuf;
+    header->rootPage = -1;
+    header->firstLeaf = -1;
+    header->numLevels = 0;
+    header->numRecords = 0;
+    header->keyType = 'i';
+    header->keyLength = sizeof(int);
+    
+    PF_UnfixPage(fd, 0, TRUE);
+    PF_CloseFile(fd);
+    return BL_OK;
+}
+
+int BL_DestroyIndex(char *fileName) {
+    return (PF_DestroyFile(fileName) == PFE_OK) ? BL_OK : BL_ERROR;
+}
+
+int BL_OpenIndex(char *fileName) {
+    int fd = PF_OpenFile(fileName, 0);
+    return (fd < 0) ? BL_ERROR : fd;
+}
+
+int BL_CloseIndex(int indexFd) {
+    return (PF_CloseFile(indexFd) == PFE_OK) ? BL_OK : BL_ERROR;
+}
